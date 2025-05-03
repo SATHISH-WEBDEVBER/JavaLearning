dd957d136a1412b01545a8f98d52a71670b364ff
SATHISH-WEBDEVBER <sathishms1589@gmail.com>
2025-05-03 13:02:34 +0530

    prg-Swapnumbers

---
+++ Day-1/SwapTwoNumbers/Swapnumbers.java
@@ -0,0 +1,16 @@
+package SwapTwoNumbers;
+
+public class Swapnumbers {
+
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
