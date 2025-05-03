bb3115ed7a47a637f10aacaccace607e26df4876
SATHISH-WEBDEVBER <sathishms1589@gmail.com>
2025-05-03 11:14:15 +0530

    prg-biggesttwonumber

--- Day-1/BiggestTwoNumbers/Bignumber.java
+++ Day-1/BiggestTwoNumbers/Bignumber.java
@@ -1,15 +1,17 @@
 package BiggestTwoNumbers;
- 
+
 class Bignumber {
-    public static void main (String args []){
+    public static void main(String args[]) {
         int a = 29;
         int b = 45;
 
-        if (a>b){
+        if (a > b) {
             System.out.println("A is the biggest Number");
-        }
-        else {
+        } else if (a < b) {
             System.out.println("B is the biggest Number");
+        } else {
+            System.out.println("Both numbers are equal");
         }
+
     }
 }
