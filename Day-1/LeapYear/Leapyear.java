530796abc7adedb0dd3f1223e956c732f97b6edd
SATHISH-WEBDEVBER <sathishms1589@gmail.com>
2025-05-03 12:54:33 +0530

    prg-Leapyear

---
+++ Day-1/LeapYear/Leapyear.java
@@ -0,0 +1,23 @@
+package LeapYear;
+
+public class Leapyear {
+    public static void main(String args[]) {
+        int year = 2002;
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println(year + " is a Leap Year.");
+                } else {
+                    System.out.println(year + " is NOT a Leap Year.");
+                }
+
+            } else {
+                System.out.println(year + " is a Leap Year.");
+            }
+        } else {
+            System.out.println(year + " is NOT a Leap Year.");
+        }
+    }
+}
+ 
